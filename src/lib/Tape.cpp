d49984af19e83173e3d76a1ab66ac118828dec77
LeonidElkin <113133848+LeonidElkin@users.noreply.github.com>
2024-05-20 12:10:22 +0300

    feat: implemented Tape interface, added ram_limit as a template

--- src/lib/Tape.cpp
+++ src/lib/Tape.cpp
@@ -4,72 +4,126 @@
 
 #include "Tape.h"
 
-uint32_t initiate_id() {
+template<size_t ram_limit>
+uint32_t Tape<ram_limit>::initiate_tape_id() {
 	std::random_device dev;
 	std::mt19937 rng(dev());
 	std::uniform_int_distribution<std::mt19937::result_type> dist;
 	return dist(rng);
 }
 
-Tape::Tape() : id(initiate_id()), current_position(0) {}
-
-Tape::Tape(const std::string &input_file) : id(initiate_id()), current_position(0) {
+template<size_t ram_limit>
+void Tape<ram_limit>::extract_page_from_ctx(const std::string &page_id) {
 	std::fstream input_stream;
+	input_stream.open("tmp/" + std::to_string(id) + "/tmp_tape" + page_id + ".tmp", std::ios::binary | std::ios::in);
+
+	if (input_stream) {
+		input_stream.read(reinterpret_cast<char *>(this->cells), sizeof(this->cells));
+		input_stream.close();
+	} else {
+		throw std::runtime_error("Couldn't open the temporary tape file during extraction from the context\n");
+	}
+}
+
+template<size_t ram_limit>
+void Tape<ram_limit>::insert_page_to_ctx(const std::string &page_id) {
+	std::fstream output_stream;
+	output_stream.open("tmp/" + std::to_string(id) + "/tmp_tape" + page_id + ".tmp", std::ios::binary | std::ios::out);
+
+	if (output_stream) {
+		output_stream.write(reinterpret_cast<char *>(this->cells), sizeof(this->cells));
+		output_stream.close();
+	} else {
+		throw std::runtime_error("Couldn't open the output tape file during insertion to the context\n");
+	}
+}
+
+template<size_t ram_limit>
+Tape<ram_limit>::Tape(size_t size) : id(initiate_tape_id()), current_position(0), size(size) {}
+
+template<size_t ram_limit>
+Tape<ram_limit>::Tape(const std::string &input_file, size_t size) : id(initiate_tape_id()), current_position(0),
+																	size(size) {
+	std::fstream input_stream;
+	if (!std::filesystem::exists(input_file)) throw std::runtime_error("Incorrect input tape was given");
 	input_stream.open(input_file, std::ios::binary | std::ios::in);
-	int32_t buffer[M];
+	int32_t buffer[ram_limit / 4];
 
 	if (input_stream) {
 
 		if (!std::filesystem::is_directory("tmp")) std::filesystem::create_directory("tmp/");
-		std::string this_tape_path = "tmp/" + std::to_string(id) + "/";
-		std::filesystem::create_directory(this_tape_path);
+		std::filesystem::create_directory("tmp/" + std::to_string(id) + "/");
 
 		for (int i = 0; !input_stream.eof(); i++) {
-
-			input_stream.read(reinterpret_cast<char *>(buffer), sizeof(buffer));
-			if (i == 1) std::copy(std::begin(buffer), std::end(buffer), std::begin(this->cells));
-
-			std::fstream output_stream;
-			output_stream.open(this_tape_path + "tmp_tape" + std::to_string(i) + ".tmp",
-							   std::ios::binary | std::ios::out);
-
-			if (output_stream) {
-				output_stream.write(reinterpret_cast<char *>(buffer), sizeof(buffer));
-				output_stream.close();
-			} else {
-				throw std::runtime_error("Couldn't open the temporary output tape file\n");
-			}
-
+			input_stream.read(reinterpret_cast<char *>(buffer), ram_limit);
+			if (i == 0) std::copy(std::begin(buffer), std::end(buffer), std::begin(this->cells));
+			else insert_page_to_ctx(std::to_string(i));
+			if (input_stream.eof() && this->size == 0) this->size = (i * ram_limit + input_stream.gcount()) / 4;
+			if (i * ram_limit / 4 > size && size != 0) break;
 		}
+
 		input_stream.close();
 	} else {
 		throw std::runtime_error("Couldn't open the tape input file\n");
 	}
 }
 
-int32_t Tape::read() const {
-	return this->cells[this->current_position];
+template<size_t ram_limit>
+Tape<ram_limit>::~Tape() {
+	std::filesystem::remove_all("tmp/" + std::to_string(id));
+	if (std::filesystem::is_empty("tmp")) std::filesystem::remove("tmp");
 }
 
-void Tape::write(int32_t new_value) {
-	this->cells[this->current_position] = new_value;
+template<size_t ram_limit>
+int32_t Tape<ram_limit>::read() const {
+	return this->cells[this->current_position % (ram_limit / 4)];
 }
 
-void Tape::move_right() {
-	if (this->current_position % M == M - 1) {
-		//Выгрузить эту страницу, загрузить другую
+template<size_t ram_limit>
+void Tape<ram_limit>::write(int32_t new_value) {
+	this->cells[this->current_position % (ram_limit / 4)] = new_value;
+}
+
+template<size_t ram_limit>
+void Tape<ram_limit>::move_right() {
+	size_t limit = ram_limit / 4;
+	if (this->current_position == this->size - 1) {
+		throw std::out_of_range("Tape position can't be more than size value\n");
+	} else if (this->current_position % limit == limit - 1) {
+		size_t page_id = this->current_position / limit;
+		insert_page_to_ctx(std::to_string(page_id));
+		page_id++;
+		if (std::filesystem::exists("tmp/" + std::to_string(id) + "/tmp_tape" + std::to_string(page_id) + ".tmp")) {
+			extract_page_from_ctx(std::to_string(page_id));
+		}
 	}
 	this->current_position++;
 }
 
-void Tape::move_left() {
+template<size_t ram_limit>
+void Tape<ram_limit>::move_left() {
+	size_t limit = ram_limit / 4;
 	if (this->current_position == 0) {
-		//Выгрузить эту страницу, загрузить другую
-	} else {
-		this->current_position--;
+		throw std::out_of_range("Tape position can't be below zero\n");
+	} else if (this->current_position % limit == 0) {
+		size_t page_id = this->current_position / limit;
+		insert_page_to_ctx(std::to_string(page_id));
+		page_id--;
+		if (std::filesystem::exists("tmp/" + std::to_string(id) + "/tmp_tape" + std::to_string(page_id) + ".tmp")) {
+			extract_page_from_ctx(std::to_string(page_id));
+		}
 	}
+	this->current_position--;
 }
 
-void Tape::algo() {
-	//Сортировка
+template<size_t ram_limit>
+Tape<ram_limit> Tape<ram_limit>::algo() {
+	//TODO: sort algo
+	return Tape("dfjkd");
 }
+
+template<size_t ram_limit>
+uint32_t Tape<ram_limit>::get_id() const {
+	return this->id;
+}
+
--- src/lib/Tape.h
+++ src/lib/Tape.h
@@ -4,16 +4,28 @@
 #include <cinttypes>
 #include <fstream>
 
-#define M 100
+#define DEFAULT_RAM_LIMIT 400
 
+template <size_t ram_limit = DEFAULT_RAM_LIMIT>
 class Tape {
+private:
 	uint32_t id;
 	size_t current_position;
-	int32_t cells[M]{0};
-public:
-	explicit Tape();
+	size_t size;
+	int32_t cells[ram_limit/4]{0};
 
-	explicit Tape(const std::string& input_file);
+	void insert_page_to_ctx(const std::string &page_id);
+
+	void extract_page_from_ctx(const std::string &page_id);
+
+	uint32_t initiate_tape_id();
+
+public:
+	explicit Tape(size_t size);
+
+	explicit Tape(const std::string &input_file, size_t size = 0);
+
+	~Tape();
 
 	[[nodiscard]] int32_t read() const;
 
@@ -23,7 +35,9 @@ public:
 
 	void move_left();
 
-	void algo();
+	Tape algo();
+
+	uint32_t get_id() const;
 };
 
 
